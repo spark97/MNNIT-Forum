c0e03109866a8818a81752fd64d6dc16d703521e
hsallbd@gmail.com <hsallbd@gmail.com>
2018-03-15 09:16:40 +0530

    Remove db password

--- function.php
+++ function.php
@@ -8,7 +8,7 @@ return $val;
 function con(){
 $dbhost="localhost";
 $dbuname="root";
-$dbpassword="12345";
+$dbpassword="";
 $dbname="forum";
 $con=new MySQLi($dbhost,$dbuname,$dbpassword,$dbname);//part of object oriented programming(mysqli=class)
 if($con->connect_errno){
